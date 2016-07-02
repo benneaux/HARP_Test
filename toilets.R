5ec90518ca54b90a5bc0d3a71058d090057ffd99
benneaux <benjamin.g.moran@gmail.com>
2016-07-02 18:19:12 +1000

    Including preliminary code to place toilets by suburb.

--- toilets.R
+++ toilets.R
@@ -1,13 +1,20 @@
 ###Setup=====
 
-#     install_packages <- c("magrittr", "dplyr", "maptools", "maps","leaflet")
-     required_packages <- list("magrittr", "dplyr", "maptools", "maps","leaflet")
+#     install_packages <- c("magrittr", "dplyr", "maptools", "maps","leaflet", "ggmap")
+     required_packages <- list("magrittr", "dplyr", "maptools", "maps","leaflet", "ggmap")
 #   
 #   install.packages(install_packages)
 # 
-    lapply(required_packages, FUN = function(required_packages) {
-      do.call("library", list(required_packages)) 
+
+load_packages <- function(x){
+  lapply(x, FUN = function(x) {
+    do.call("library", 
+            list(x)
+            )
     })
+}
+
+load_packages(required_packages)
 
 ###Import the data=====
 
@@ -17,9 +24,30 @@
 
     # If you want, you can run these to save a copy to your local wd.
     
-     write.csv(toilet_df_import, file = "toilet_data.csv")
-     toilet_df <- read.csv('toilet_data.csv')
+    # This code is my initial attempt, before grouping the toilets together by suburb.
+#        write.csv(toilet_df_import, 
+#                  file = "toilet_data.csv"
+#                  )
+#        toilet_df <- read.csv('toilet_data.csv')
+     
+     # This is my next attempt, where I pipe the suburb into another function that returns an approximate lat/lng for that suburb.
     
+      # First, create the data frame.
+    
+        write.csv(toilet_df_import,
+                   file = "toilet_suburb_data.csv"
+                   )
+         toilet_suburb_df <- read.csv('toilet_suburb_data.csv')
+         
+      # Next, the function for returning lat/lng for each suburb.
+        
+         map_suburb_to_latlng <- toilet_suburb_df %>%
+           suburb <- toilet_suburb_df$Town %>%
+           mutate(suburb_lat <- geocode(paste(suburb, ", Australia"))$lat) %>%
+           mutatate(suburb_lon <- geocode(paste(suburb, ", Australia"))$lon)
+         
+          
+         
     # Create the labels
     
      toilet_df$hover <- paste(toilet_df$Name, ", ", toilet_df$Town)
