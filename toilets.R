eccacfe833245ce122afb30a9893c836aef9b40b
benneaux <benjamin.g.moran@gmail.com>
2016-07-05 13:31:09 +1000

    This version will handle both OSX and Windows. Will also check which packages need installing. Will also check if data needs to be downloaded.

--- toilets.R
+++ toilets.R
@@ -1,45 +1,96 @@
 ###Setup===========================================================================================
-  
-  ## Install Packages
-    # install_packages <- c("magrittr", "dplyr", "maptools", "maps","leaflet", "ggmap")
-    # install.packages(install_packages)
-     
-  ## Load Package
-    
-    required_packages <- list("magrittr", "dplyr", "maptools", "maps","leaflet", "ggmap")
-    
-    load_packages <- function(x) {
-      lapply(x, FUN = function(x) {
-        do.call("require", 
-                list(x)
-                )
-      })
-    }
-    
-    load_packages(required_packages)
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform.type
+    if (grepl("^darwin", R.version))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version))
+      os <- "linux"
+  }
+  tolower(os)
+}
+
+install_packages <- c("magrittr", "dplyr", "maptools", "maps","leaflet", "ggmap")
+required_packages <- list("magrittr", "dplyr", "maptools", "maps","leaflet", "ggmap")
+new_packages <- install_packages[!(install_packages %in% installed.packages()[,"Package"])]
+load_packages <- function(x) {
+  lapply(x, FUN = function(x) {
+    do.call("require", 
+            list(x)
+    )
+  })
+}
+
+if (length(new_packages)) {
+  install.packages(new_packages)
+  load_packages(required_packages)
+} else {
+  load_packages(required_packages)
+}
+
+
+data_url <- ("http://data.gov.au/dataset/553b3049-2b8b-46a2-95e6-640d7986a8c1/
+              resource/bb22debd-95a6-4984-882c-12b58c92e16c/download/Latest-CSV-Export.csv")
 
 ###Import the data (Basic Data)====================================================================
 
-# This code is my initial attempt, before grouping the toilets together by suburb.
-
-  ## Run this first to copy all the data from data.gov.au
+  ## This code is my initial attempt, before grouping the toilets together by suburb.
     
-      data_url <-  "http://data.gov.au/dataset/553b3049-2b8b-46a2-95e6-640d7986a8c1/resource/bb22debd-95a6-4984-882c-12b58c92e16c/download/Latest-CSV-Export.csv"
-      toilet_df_import <- read.csv(data_url)
-
-  ## If you want, you can run these to save a copy to your local wd.
+    wd <- getwd() # Declare the working directory as wd.
     
-
-      wd <- getwd()  
+    # Set the location that I would like to save the file/the file is already located. (OSX)
+    
+      osx_data_path <- paste(
+        wd,
+        "/Data/toilet_data.csv",
+        sep = ""
+      )
       
-      write.csv(toilet_df_import, 
-                  file = paste(wd,"/Data/toilet_data.csv", sep = "")
-                  )
+    # Set the location that I would like to save the file/the file is already located. (Windows)  
       
-      toilet_df <- read.csv(paste(wd,"/Data/toilet_data.csv", sep = ""))
+      windows_data_path <- paste(
+        wd,
+        "\\Data\\toilet_data.csv",
+        sep = ""
+      )
+    
+    
+    # This will run get_os() to determine the OS, then it will copy the data from data_url
+    # IFF the data does not already exist (as specified by either osx_data_path/windows_data_path)
 
-    # Choose how you want to fileter the data. If you're running it for Melbourne only
-    # you can filter additionally by postcodes, if you know what they are.
+       if(get_os() == "osx") {
+         if (file.exists(osx_data_path)) {
+           toilet_df <- read.csv(osx_data_path)
+           break
+           } else {
+             toilet_df_import <- read.csv(data_url)
+             write.csv(
+               toilet_df_import,
+               file = osx_data_path
+             )
+           }
+       } else {
+         if (file.exists(windows_data_path)) {
+           toilet_df <- read.csv(windows_data_path)
+           break
+         } else {
+           toilet_df_import <- read.csv(data_url)
+           write.csv(
+             toilet_df_import,
+             file = windows_data_path
+           )
+         }
+       }
+    
+### Filter the Data================================================================================
+
+## Choose how you want to fileter the data. If you're running it for Melbourne only
+## you can filter additionally by postcodes, if you know what they are.
       
       State <- "Victoria"
       
@@ -96,22 +147,51 @@
       toilet_df$hover <- paste(toilet_df$Name, ", ", toilet_df$Town)
 
 ###Make the map====================================================================================
-  centre_map_on <- geocode("Melbourne, Australia")
-           
-  map <- leaflet(toilet_df) %>% # Call leaflet() using our data
+  
+  # Declare where you want to centre the map. geocode() can handle text inputs but remember to 
+  # specify the country - as I have - to avoid any confusion.
+      
+      centre_map_on <- geocode("Melbourne, Australia")
+
+  # Call leaflet using our data  
+      
+    map <- leaflet( 
+      toilet_df
+      ) %>%
+      
+      # Add default OpenStreetMap map tiles
+      
+      addTiles() %>%  
+      
+      # Centre the map on the location specified by centre_map_on.
+      
+      setView(
+        lng = centre_map_on$lon,
+        lat = centre_map_on$lat,
+        
+        # change zoom value to set starting zoom: 1 == whole globe; 14 ≈ suburb level. 
+        zoom = 12
+        ) %>%
+      
+      # Tell it where to get the labels.      
+      
+      addMarkers(
+        popup=as.character(
+          toilet_df$hover
+          ),
+        
+        # clusterOptions tells the map to group markers at different zoom levels; comment this 
+        # line out if you wish to see all markers at every zoom level.
+        clusterOptions = markerClusterOptions()
+        )
     
-    addTiles() %>%  # Add default OpenStreetMap map tiles
+      map # Call the map.    
     
-    setView(centre_map_on$lon, centre_map_on$lat, zoom = 12) %>%
+## I'm behind a proxy at work, so I needed to save the map as a html and then open it in my browser. 
+## You probably won't have to, in which case just run the Make the Map chunk above.
+
+###Export to html==================================================================================          
     
-    addMarkers(popup=as.character(toilet_df$hover), # Tell it where to get the labels.
-               clusterOptions = markerClusterOptions() # clusterOptions tells the map to group markers.
-               )
-    
-    map # Call the map.    
-    
-## I'm behind a proxy, so I needed to save the map as a html and then open it in my browser. You might not have to.
-    
-    htmlwidgets::saveWidget(map, file='map.html')
+      htmlwidgets::saveWidget(map, file='map.html')
 
 ### Done
